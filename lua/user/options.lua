51fe1efe342a990484ae7a0453ff5dbeda4e8885
Shaun Cheng <shaunchenggy@gmail.com>
2022-02-13 21:29:33 +0800

    minor changes (ignore)

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -31,7 +31,7 @@ local options = {
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = true,                             -- do not display lines as one long line
-  scrolloff = 5,                           -- is one of my fav
+  scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
 }
 
